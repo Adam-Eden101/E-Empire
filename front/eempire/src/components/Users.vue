879203a0bfda0ace4513c892e490e2afda91708c
Cameron CARRA <carra_c@etna-alternance.net>
2021-05-12 15:20:20 +0200

    initial front commit + début user vue avec Axios

---
+++ front/eempire/src/components/Users.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <p>Users</p>
+        <p v-for="user in users" :key="user.id">{{ user.username }}</p>
+    </section>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+    name: "Users",
+    data() {
+        return {
+            users: [],
+        }
+    },
+    async mounted() {
+        const response = await axios.get('http://localhost:3000/users');
+        this.users = response.data;
+    }
+}
+
+</script>
